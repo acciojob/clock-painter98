9c2b3052ea608a35cb09f4921b4d631002fafbaf
Pallavi <120320713+painter98@users.noreply.github.com>
2023-06-29 15:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ let hour = time.getHours()>=12?time.getHours()-12:time.getHours();
 let minute = time.getMinutes();
 let second = time.getSeconds();
 
-timer.innerText = `${date}, ${hour}:${minute>10?minute:'0'+minute}:${second>10?second:'0'+second} ${time.getHours()>12?'PM':'AM'}`
+timer.innerText = `${date}, ${hour}:${minute>10?minute:'0'+minute}:${second>=10?second:'0'+second} ${time.getHours()>12?'PM':'AM'}`
 },[1000])
