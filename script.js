eb1796a4948dcad56f90b1d2fc8a11fefe4b6afa
Pallavi <120320713+painter98@users.noreply.github.com>
2023-06-29 15:26:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+ let timer = document.getElementById('timer');
+
+let time;
+setInterval(()=>{
+    time = new Date();
+let date = `${time.getDate()}/${time.getMonth()}/${time.getFullYear()}`;
+let hour = time.getHours()>=12?time.getHours()-12:time.getHours();
+let minute = time.getMinutes();
+let second = time.getSeconds();
+
+timer.innerText = `${date}, ${hour}:${minute>10?minute:'0'+minute}:${second>10?second:'0'+second} ${time.getHours()>12?'PM':'AM'}`
+},[1000])
